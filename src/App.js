afce0ca38adf06a464ae5867f41da424c6ba4116
Tyler Thompson <th0mps0n.ty@protonmail.com>
2018-04-04 17:31:16 -0400

    checkpoint-2 finished - app is showing custom data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import ToDo from './components/ToDo'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Walk the dog', isCompleted: true },
+        { description: 'Take out the trash', isCompleted: false },
+        { description: 'Buy new shoes', isCompleted: false }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <ul>
-          <ToDo />
-          <ToDo />
+          { this.state.todos.map( (todo, index) =>
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+          )}
         </ul>
       </div>
     );
